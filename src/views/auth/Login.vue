c33c94f84478a6fe882640248cffeeab4ade2aef
jackstraw <jackstraw@jackstraws-Mac-mini.local>
2018-01-17 08:19:16 +0800

    login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,28 +1,72 @@
 <template>
-  <el-form>
-    <el-form-item label="用户名" prop="username">
-      <el-input type="text"></el-input>
-    </el-form-item>
-    <el-form-item label="密码" prop="password">
-      <el-input type="password"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary">登录</el-button>
-    </el-form-item>
-  </el-form>
+  <div class="card">
+    <div class="card-header">
+      <div class="card-header-title has-text-centered">登录</div>
+    </div>
+    <div class="card-content">
+      <el-form :model="form" :statusIcon="true" :rules="rules" ref="form">
+        <el-form-item label="用户名" prop="username">
+          <el-input type="text" v-model="form.username" autoComplete="on"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="form.password" autoComplete="on"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="login('form')">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 <script>
   export default {
+    data () {
+      var checkUserName = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('用户名不能为空'))
+        }
+        callback()
+      }
+      var checkPassword = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('请输入密码'))
+        }
+        callback()
+      }
+      return {
+        form: {
+          username: '',
+          password: ''
+        },
+        rules: {
+          username: [
+            { validator: checkUserName, trigger: 'blur' }
+          ],
+          password: [
+            { validator: checkPassword, trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      login (ref) {
+        this.$refs[ref].validate((valid) => {
+          if (valid) {
+            this.$auth.login()
+          } else {
+            return false
+          }
+        })
+      }
+    }
   }
 </script>
 <style lang="scss" scoped>
-  .is-black {
-    opacity: 0.5;
-  }
   .card {
     width: 500px;
     margin: auto auto;
     position: relative;
     top: 100px;
   }
+
 </style>
